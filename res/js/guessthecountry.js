84ca47083e05ce4b18c9fcdec15b3897592f50f9
Štěpán Žďánský <zdanskystepanek@gmail.com>
2024-02-24 23:51:11 +0100

    save

--- res/js/guessthecountry.js
+++ res/js/guessthecountry.js
@@ -1,7 +1,43 @@
-const loadData = async()=>{
+
+
+const body = document.getElementById("country")
+//SPRÁVNÁ ODPOVĚĎ NA OTÁZKU--------------------------
+let correctValue = "";
+let questionNumber = 0;
+
+
+
+
+//USER ODPOVĚĎ NA OTÁZKU-----------------------------
+let answerValue = "";
+//co napise user do divu
+const userWord = document.getElementById("userWord");
+
+
+//HIDE ELEMENT----------------------------------------
+const hideElement = (element) => {
+    element.style.display = "none";
+};
+//SHOW ELEMENT----------------------------------------
+const showElement = (element) => {
+    element.style.display = "initial"};
+  
+
+//kdyz user da enter
+userWord.addEventListener("keypress", (e) => {
+    if (e.key === 'Enter') {
+     console.log("jooooo");
+     e.preventDefault();
+     e .currentTarget.value = "";
+    }
+    
+
+});
+
+//GAME----------------------
+const buildGame = async() =>{
     const file = await fetch("./res/data/guessthecountry.json");
     const data = await file.json();
-};
 
 let generatedNumbers = [];
 function generateRandomNumber() {
@@ -18,5 +54,20 @@ function generateRandomNumber() {
     generatedNumbers.push(randomNumber); // Přidání číslo do pole generovaných čísel
     return randomNumber;
     }
-console.log(generatedNumbers);
-console.log(generateRandomNumber);
+
+    let actualNum = generateRandomNumber();
+    console.log(generatedNumbers);
+    if (questionNumber < 10) {
+            /*vypíše do konzole jakou odpověď uživatel zvolil*/
+    console.log("Odpověď: "+answerValue);
+
+    /*zobrazí se aktuální otázka*/
+    body.innerText = data[actualNum].question;
+    /*js zjistí správnou odpověď na otázku*/
+    correctValue = data[actualNum].country;
+    return correctValue;
+    }
+    else{
+        
+    } 
+};
