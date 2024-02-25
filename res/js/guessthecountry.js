02899d497cb04a2abb817fef2dfb4d697deb2cf6
Štěpán Žďánský <zdanskystepanek@gmail.com>
2024-02-25 21:47:22 +0100

    save

--- res/js/guessthecountry.js
+++ res/js/guessthecountry.js
@@ -1,17 +1,29 @@
 
-
-const body = document.getElementById("country")
+const start = document.getElementById("startBtn");
+const body = document.getElementById("country");
 //SPRÁVNÁ ODPOVĚĎ NA OTÁZKU--------------------------
 let correctValue = "";
 let questionNumber = 0;
 
 
+/*
+//Class
+class Guessthecountry{
+    constructor(countryID, country){
+        this.countryID = countryID;
+        this.name = this.country;
+        this.img = new Image();
+        this.img.src = this.path;
+        console.log(this.img.src);
+    }
 
-
+}
+*/
 //USER ODPOVĚĎ NA OTÁZKU-----------------------------
 let answerValue = "";
 //co napise user do divu
-const userWord = document.getElementById("userWord");
+const input = document.getElementById("input");
+const inputValue = input.value;
 
 
 //HIDE ELEMENT----------------------------------------
@@ -21,24 +33,33 @@ const hideElement = (element) => {
 //SHOW ELEMENT----------------------------------------
 const showElement = (element) => {
     element.style.display = "initial"};
-  
+ 
+
+//tady konec
+function fn1() {
+    answerValue = inputValue;
+    
+}
 
 //kdyz user da enter
-userWord.addEventListener("keypress", (e) => {
+input.addEventListener("keypress", (e) => {
     if (e.key === 'Enter') {
-     console.log("jooooo");
+    fn1();
+    //smaze to co je napsane v poli kdyz user zmackne enter
      e.preventDefault();
-     e .currentTarget.value = "";
+     e.currentTarget.value = "";
     }
-    
+
 
 });
 
+
 //GAME----------------------
-const buildGame = async() =>{
-    const file = await fetch("./res/data/guessthecountry.json");
+const buildGame = async () =>{
+    const file = await fetch("/res/data/guessthecountry.json");
     const data = await file.json();
 
+//GENERATING RANDOM NUMBNERS----------------------
 let generatedNumbers = [];
 function generateRandomNumber() {
     
@@ -56,18 +77,20 @@ function generateRandomNumber() {
     }
 
     let actualNum = generateRandomNumber();
-    console.log(generatedNumbers);
+    console.log( "cislo otazky:" + actualNum);
     if (questionNumber < 10) {
             /*vypíše do konzole jakou odpověď uživatel zvolil*/
-    console.log("Odpověď: "+answerValue);
+    console.log("Odpověď: " + answerValue);
 
     /*zobrazí se aktuální otázka*/
-    body.innerText = data[actualNum].question;
+    //body.innerHTML = new Guessthecountry(actualNum);
     /*js zjistí správnou odpověď na otázku*/
     correctValue = data[actualNum].country;
+    console.log(correctValue);
     return correctValue;
     }
     else{
         
     } 
 };
+
